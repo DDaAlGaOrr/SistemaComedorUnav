b5753a6deeeb82158c4dde828bb15294409ad2a6
Daniel Gamez Orozco <daalgaor.17@gmail.com>
2022-11-22 18:02:24 -0700

    add oracle connection and create query select

--- database/connection.js
+++ database/connection.js
@@ -1,18 +1,7 @@
 const oracledb = require('oracledb');
-const dotenv = require('dotenv').config();
 
-const connection = oracledb.getConnection(
-  {
-    user          : 'system',
-    password      : 'oracle',
-    connectString : "192.168.1.31:49161" 
-  },(err, connection)=>{
-    if (err) {
-      console.error(err.message);
-      return;
-    }
-    console.log("Conectado")
-    return connection
-  });
 
-module.exports = connection
+
+
+
+    
--- server.js
+++ server.js
@@ -1,63 +1,90 @@
 const express = require('express');
 const app = express()
-// const connection = require('./database/connection')
+const oracledb = require('oracledb');
 
 app.set('view engine','ejs');
 app.use(express.urlencoded({extended:false}));
 app.use(express.json());
 app.use(express.static('public'));
 
-app.get('/',(req,res)=>{
-    res.render('index.ejs')
-})
+try {
+    const createConnection = async()=>{
+      const connection = await oracledb.getConnection({
+        user: "system", 
+        password: "oracle",
+        connectionString: "192.168.1.31:49161/" 
+      }) 
 
-app.post('/internoDesayuno',(req,res)=>{
-    const fecha = req.body.date
-    const matricula = req.body.matricula
+      app.get('/',(req,res)=>res.render('index.ejs'))
+    
+    app.post('/internoDesayuno',(req,res)=>{
+        const fecha = req.body.date
+        const matricula = parseInt(req.body.matricula)
+        const queryAlumnosInternosInscritos = `SELECT DISTINCT AE.CODIGO_PERSONAL, AP.NOMBRE ||' '|| AP.APELLIDO_PATERNO AS NOMBRE FROM ALUM_ESTADO AE
+        JOIN ALUM_ACADEMICO AA ON AE.CODIGO_PERSONAL = AA.CODIGO_PERSONAL AND AA.RESIDENCIA_ID = 'I'
+        JOIN CARGA C ON AE.CARGA_ID = C.CARGA_ID AND C.F_INICIO <= NOW AND C.F_FINAL >= NOW
+        JOIN ALUM_PERSONAL AP ON AE.CODIGO_PERSONAL = AP.CODIGO_PERSONAL
+        WHERE AE.CODIGO_PERSONAL = ${matricula} AND AE.ESTADO = 'I'`
 
-    console.log(`${fecha} ${matricula}`)
-})
-app.post('/externoDesayuno',(req,res)=>{
-    console.log(req.body)
-    console.log('desayuno externo')
-})
-app.post('/visitaDesayuno',(req,res)=>{
-    console.log(req.body)
-    console.log('desayuno visita')
+        connection.execute(queryAlumnosInternosInscritos,(error,results)=>{
+            if(error){
+                console.log(error)
+            }
+            else{
+                console.log(results.rows)
+            }
+        })
+        console.log(`${fecha} ${typeof matricula}`)
+    })
+    app.post('/externoDesayuno',(req,res)=>{
+        console.log(req.body)
+        console.log('desayuno externo')
+    })
+    app.post('/visitaDesayuno',(req,res)=>{
+        console.log(req.body)
+        console.log('desayuno visita')
+    
+    })
+    
+    
+    app.post('/internoComida',(req,res)=>{
+        const fecha = req.body.date
+        const matricula = req.body.matricula
+    
+        console.log(`${fecha} ${matricula}`)
+    })
+    app.post('/externoComida',(req,res)=>{
+        console.log(req.body)
+        console.log('Comida externo')
+    })
+    app.post('/visitaComida',(req,res)=>{
+        console.log(req.body)
+        console.log('Comida visita')
+    })
+    
+    
+    app.post('/internoCena',(req,res)=>{
+        const fecha = req.body.date
+        const matricula = req.body.matricula
+    
+        console.log(`${fecha} ${matricula}`)
+    })
+    app.post('/externoCena',(req,res)=>{
+        console.log(req.body)
+        console.log('Cena externo')
+    })
+    app.post('/visitaCena',(req,res)=>{
+        console.log(req.body)
+        console.log('Cena visita')
+    })
+    
+      console.log("Conectado")
+    }
+    createConnection()
+  } catch (error) {
+    console.log(error)
+  }
 
-})
-
-
-app.post('/internoComida',(req,res)=>{
-    const fecha = req.body.date
-    const matricula = req.body.matricula
-
-    console.log(`${fecha} ${matricula}`)
-})
-app.post('/externoComida',(req,res)=>{
-    console.log(req.body)
-    console.log('Comida externo')
-})
-app.post('/visitaComida',(req,res)=>{
-    console.log(req.body)
-    console.log('Comida visita')
-})
-
-
-app.post('/internoCena',(req,res)=>{
-    const fecha = req.body.date
-    const matricula = req.body.matricula
-
-    console.log(`${fecha} ${matricula}`)
-})
-app.post('/externoCena',(req,res)=>{
-    console.log(req.body)
-    console.log('Cena externo')
-})
-app.post('/visitaCena',(req,res)=>{
-    console.log(req.body)
-    console.log('Cena visita')
-})
 
 
 app.listen(3000, ()=>{
